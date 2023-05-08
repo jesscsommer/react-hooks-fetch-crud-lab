0ac4f35300ba99dc9e1409d875a3e34059baad91
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-05-08 13:12:13 -0700

    Add new questions via POST request

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 
 function QuestionForm(props) {
-  const [formData, setFormData] = useState({
+  const initialState = {
     prompt: "",
     answer1: "",
     answer2: "",
     answer3: "",
     answer4: "",
     correctIndex: 0,
-  });
+  }
+
+  const [formData, setFormData] = useState(initialState);
 
   function handleChange(event) {
     setFormData({
@@ -20,6 +22,23 @@ function QuestionForm(props) {
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
+
+    fetch('http://localhost:4000/questions/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        'prompt': formData.prompt,
+        'answers': [formData.answer1, formData.answer2, formData.answer3, formData.answer4],
+        'correctIndex': formData.correctIndex
+      })
+    })
+    .then(res => res.json())
+    .then(data => {
+      props.addQuestion(data)
+      setFormData(initialState)
+    })
   }
 
   return (
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,14 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
-  const [questions, setQuestions] = useState([])
-
-  useEffect(() => {
-    fetch('http://localhost:4000/questions')
-    .then(res => res.json())
-    .then(data => setQuestions(data))
-  }, [])
+function QuestionList({ questions }) {
 
   return (
     <section>
